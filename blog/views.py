432a789632541bae19cf796dc65542857d286959
Esther Zinn <Bionicfemme@me.com>
2015-10-21 15:15:02 -0400

    resolving pull conflict

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -9,3 +10,19 @@ def post_list(request):
 def post(request, pk):
 	post = Post.objects.get(pk=int(pk))
 	return render(request, 'blog/post.html', {'post': post})
+
+def listing(request):
+    post_list = Posts.objects.all()
+    paginator = Paginator(post_list, 1) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
+
+    return render_to_response('list.html', {"posts": posts})
