d91831601799c7084dd525f039b34892b7c9d5cf
bash (curl -sL https://raw.githubusercontent.com/ga-instructors/installfest_script/pluto/builds/mac) <edward.petner@gmail.com>
2015-10-21 12:04:25 -0400

    edi splaying

---
+++ mysite/sql/blog.sql
@@ -0,0 +1,14 @@
+INSERT INTO blog (title, text, created_date, published_date, author) 
+VALUES ("My First Post!", "This is my first post and I rock!", timestamp, timestamp, "Ed Petner");
+
+INSERT INTO blog (title, text, created_date, published_date, author) 
+VALUES ("My Second Post!", "This is my second post and I rock!", timestamp, timestamp, "Ed Petner");
+
+INSERT INTO blog (title, text, created_date, published_date, author) 
+VALUES ("My Thirdish Post!", "This is my third post and I rock!", timestamp, timestamp, "Ed Petner");
+
+INSERT INTO blog (title, text, created_date, published_date, author) 
+VALUES ("My Fourthish Post!", "This is my Fourth post and I rock!", timestamp, timestamp, "Ed Petner");
+
+INSERT INTO blog (title, text, created_date, published_date, author) 
+VALUES ("My Fifth Post!", "This is my 5th post and I rock!", timestamp, timestamp, "Ed Petner");
